c811fb523abc04cdc10b7efed866179f48ff8e55
David Négrier <david@davidtcm>
2012-09-02 23:35:34 +0200

    Fixing usage for Mouf2

---
+++ src/splash.php
@@ -0,0 +1,22 @@
+<?php
+// First thing first, let's include the Mouf configuration:
+// (only if we are not in admin mode)
+/*require_once dirname(__FILE__).'/../../../../mouf/MoufManager.php';
+if (!MoufManager::hasHiddenInstance()) {
+	require_once dirname(__FILE__).'/../../../../Mouf.php';
+}*/
+use Mouf\MoufManager;
+
+require_once __DIR__.'/../../../autoload.php';
+require_once __DIR__.'/../../../../mouf/Mouf.php';
+
+
+$splash = MoufManager::getMoufManager()->getInstance('splash');
+
+if (!isset($splashUrlPrefix)) {
+	$splashUrlPrefix = ROOT_URL;
+}
+
+$splash->route($splashUrlPrefix);
+
+?>
