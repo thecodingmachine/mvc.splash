093454e6fc73f974405673741b5f4da2a08888fb
David Négrier <david@davidtcm>
2012-09-04 14:35:24 +0200

    Keeping debugging for Mouf2 support

--- src/splash.php
+++ src/splash.php
@@ -7,7 +7,7 @@ if (!MoufManager::hasHiddenInstance()) {
 }*/
 use Mouf\MoufManager;
 
-require_once __DIR__.'/../../../autoload.php';
+//require_once __DIR__.'/../../../autoload.php';
 require_once __DIR__.'/../../../../mouf/Mouf.php';
 
 
