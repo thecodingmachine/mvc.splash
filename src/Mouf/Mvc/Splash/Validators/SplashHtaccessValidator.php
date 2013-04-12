ee321ac77ccc49cfa27fdf4ada41f96f5911d50b
Pierre Vaidie <pierre.vaidie@gmail.com>
2013-04-12 12:12:51 +0200

    Fix doc

--- src/Mouf/Mvc/Splash/Validators/SplashHtaccessValidator.php
+++ src/Mouf/Mvc/Splash/Validators/SplashHtaccessValidator.php
@@ -13,7 +13,7 @@ class SplashHtaccessValidator implements MoufStaticValidatorInterface {
 	 */
 	public static function validateClass() {
 		
-		if (file_exists(ROOT_PATH.".htaccess")) {
+		if (!file_exists(ROOT_PATH.".htaccess")) {
 			return new MoufValidatorResult(MoufValidatorResult::WARN, "Unable to find Splash .htaccess file. You should <a href='".MOUF_URL."splashApacheConfig/'>configure the Apache redirection</a>.");
 		} else {
 			return new MoufValidatorResult(MoufValidatorResult::SUCCESS, ".htaccess file found.");
