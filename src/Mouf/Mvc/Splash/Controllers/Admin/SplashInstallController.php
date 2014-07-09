5322023a298e97b9a6dc3d9d53d864ece2a29950
Jeremie@JeremieTCM <j.pose@thecodingmachine.com>
2014-07-09 11:07:49 +0200

    add new extensions in htaccess file generation for fonts access

--- src/Mouf/Mvc/Splash/Controllers/Admin/SplashInstallController.php
+++ src/Mouf/Mvc/Splash/Controllers/Admin/SplashInstallController.php
@@ -291,7 +291,7 @@ class SplashInstallController extends Controller {
 		$this->exludeExtentions = $moufManager->getVariable("splashexludeextentions");
 		$this->exludeFolders = $moufManager->getVariable("splashexludefolders");
 		if (empty($this->exludeExtentions)){
-			$this->exludeExtentions = array("js", "ico", "gif", "jpg", "png", "css");
+			$this->exludeExtentions = array("js", "ico", "gif", "jpg", "png", "css", "woff", "ttf", "svg", "eot");
 		}
 		if (empty($this->exludeFolders)){
 			$this->exludeFolders = array("vendor");
