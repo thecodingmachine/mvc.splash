f04ee9d5539fa0af54655185cc423115708f84f1
David Négrier <d.negrier@thecodingmachine.com>
2015-11-17 16:58:59 +0100

    SplashMiddleware is moved to splash-common

---
+++ src/Mouf/Mvc/Splash/SplashMiddlewareValidator.php
@@ -0,0 +1,24 @@
+<?php
+namespace Mouf\Mvc\Splash;
+
+use Mouf\Validator\MoufValidatorResult;
+use Mouf\Validator\MoufStaticValidatorInterface;
+use Mouf\MoufManager;
+
+class SplashMiddlewareValidator implements MoufStaticValidatorInterface
+{
+    /**
+     * Check if an instance named 'splashMiddleware' actually exists
+     * @return \Mouf\Validator\MoufValidatorResult
+     */
+    public static function validateClass()
+    {
+        $instanceExists = MoufManager::getMoufManager()->instanceExists('splashMiddleware');
+
+        if ($instanceExists) {
+            return new MoufValidatorResult(MoufValidatorResult::SUCCESS, "'splashMiddleware' instance found");
+        } else {
+            return new MoufValidatorResult(MoufValidatorResult::WARN, "Unable to find the 'splashMiddleware' instance. Please run the installer or click here to <a href='".MOUF_URL."mouf/newInstance2?instanceName=splash&instanceClass=Mouf\\Mvc\\Splash\\SplashMiddleware'>create an instance of the SplashMiddleware class named 'splashMiddleware'</a>.");
+        }
+    }
+}
