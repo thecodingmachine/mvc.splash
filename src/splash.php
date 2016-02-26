4becb699e2443af55e64b863ac04fd3b684df6e5
David Négrier <d.negrier@thecodingmachine.com>
2016-02-26 15:08:42 +0100

    cleaning code

--- src/splash.php
+++ src/splash.php
@@ -1,10 +1,6 @@
 <?php
 // First thing first, let's include the Mouf configuration:
 // (only if we are not in admin mode)
-/*require_once dirname(__FILE__).'/../../../../mouf/MoufManager.php';
-if (!MoufManager::hasHiddenInstance()) {
-    require_once dirname(__FILE__).'/../../../../Mouf.php';
-}*/
 use Mouf\MoufManager;
 use Zend\Diactoros\Server;
 use Mouf\Mvc\Splash\Splash;
@@ -15,7 +11,6 @@ if (isset($_SERVER['BASE'])) {
     define('ROOT_URL', "/");
 }
 
-//require_once __DIR__.'/../../../autoload.php';
 require_once __DIR__.'/../../../../mouf/Mouf.php';
 
 $splash = MoufManager::getMoufManager()->getInstance('splashMiddleware');
@@ -30,12 +25,4 @@ if (isset($_SERVER["CONTENT_TYPE"]) && strpos($_SERVER["CONTENT_TYPE"], 'json'))
 /** @var $splash Splash */
 $server = Server::createServer($splash, $_SERVER, $_GET, $postdata, $_COOKIE, $_FILES);
 
-// json_decode postdata if content_type is json (application/json, ...)
-/*if ($request->getContentType() === 'json') {
-    $postdata = file_get_contents("php://input");
-    $postdata = json_decode($postdata, true);
-    $request->request = new ParameterBag($postdata);
-}*/
-
-// TODO: add a finalhandler for 404 management (or not...)
 $server->listen();
